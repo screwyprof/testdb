53d713c789db967f69bcd48d699b8dfd4779dd00
Max O'Donnell <screwyprof@gmail.com>
2020-05-03 20:11:42 +0100

    Read DATABASE_URL from env

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
@@ -9,25 +10,45 @@ module Lib
     ) where
 
 import Data.ByteString (ByteString)
+import GHC.Generics
+
+import Control.Exception
+import System.Envy
 import Database.PostgreSQL.Simple
 
 import Db
 
+newtype PGConfig = PGConfig {
+    databaseUrl :: ByteString -- "DATABASE_URL"
+  } deriving (Generic, Show)
+
+instance FromEnv PGConfig
+
 someFunc :: IO ()
-someFunc =
-    withDbConn connURI $ \conn -> do
-        let newUser = User { userId = -1
-                           , userName="CoolUser"
-                           , userEmail="cool@cool.net"
-                           , userPassword="qwerty123"
-                           }
+someFunc = do
+    pgCfg <- decodeWithDefaults defaultPGConfig
+    manageUsers pgCfg
+
+manageUsers :: PGConfig -> IO ()
+manageUsers pgCfg =
+    catch (withDbConn (databaseUrl pgCfg) $ \conn -> do
         createUser conn newUser
-        showUsers conn
+        showUsers conn)
+    (\e -> do 
+        let err = show (e :: IOException)
+        putStr $ "Cannot connect to the database: " <> err)
+
+newUser :: User
+newUser = User { userId = -1
+               , userName="CoolUser"
+               , userEmail="cool@cool.net"
+               , userPassword="qwerty123"
+               }
 
 createUser :: Connection -> User -> IO ()
-createUser conn newUser = do 
+createUser conn nu = do 
     putStrLn "Creating user..."
-    addedUser <- addUser conn newUser
+    addedUser <- addUser conn nu
     case addedUser of 
         Left err -> putStrLn $ "Cannot add user: " <> err
         Right u -> showUser conn $ userId u
@@ -43,5 +64,5 @@ showUsers conn = do
     putStrLn "Users:"
     mapM_ print =<< fetchUsers conn
 
-connURI :: ByteString
-connURI = "postgres://postgres@localhost/verisart"
+defaultPGConfig :: PGConfig
+defaultPGConfig = PGConfig "postgres://postgres@localhost/test?sslmode=disable"
